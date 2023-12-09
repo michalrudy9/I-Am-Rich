c1b49ba15f8aeaea981abbcf5ec0f93f642d1a44
michalrudy9 <michallrudy9@gmail.com>
2023-12-09 20:24:03 +0100

    Initial Commit

---
+++ I Am Rich/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  I Am Rich
+//
+//  Created by Michał Rudy on 09/12/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
